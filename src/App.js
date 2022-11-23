18f1ec024235f808126f0fd4256c0d15ea3dd20c
Kundan <kundan.karna1994@gmail.com>
2022-11-23 20:10:11 +0545

    intialized with ui and auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+    defer,
+} from "react-router-dom";
+import { LoginPage } from "./pages/Login";
+import { HomePage } from "./pages/Home";
+import { ProfilePage } from "./pages/Profile";
+import { SettingsPage } from "./pages/Settings";
+import { ProtectedLayout } from "./components/ProtectedLayout";
+import { HomeLayout } from "./components/HomeLayout";
+import "./styles.css";
+import { AuthLayout } from "./components/AuthLayout";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+// ideally this would be an API call to server to get logged in user data
+
+const getUserData = () =>
+    new Promise((resolve) =>
+        setTimeout(() => {
+            const user = window.localStorage.getItem("user");
+            resolve(user);
+        }, 3000)
+    );
+
+// for error
+// const getUserData = () =>
+//   new Promise((resolve, reject) =>
+//     setTimeout(() => {
+//       reject("Error");
+//     }, 3000)
+//   );
+
+export const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route
+            element={<AuthLayout />}
+            loader={() => defer({ userPromise: getUserData() })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+            <Route element={<HomeLayout />}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+            </Route>
 
-export default App;
+            <Route path="/dashboard" element={<ProtectedLayout />}>
+                <Route path="profile" element={<ProfilePage />} />
+                <Route path="settings" element={<SettingsPage />} />
+            </Route>
+        </Route>
+    )
+);
